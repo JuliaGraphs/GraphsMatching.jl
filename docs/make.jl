19546ec100dd1dc6b93722bf8a0d2af89ab01de8
mbesancon <mathieu.besancon@gmail.com>
2018-05-28 12:19:48 -0400

    deployable docs

--- docs/make.jl
+++ docs/make.jl
@@ -10,3 +10,12 @@ makedocs(
 		"Getting started"         => "index.md",
     ]
 )
+
+deploydocs(
+    deps        = nothing,
+    make        = nothing,
+    repo        = "github.com/JuliaGraphs/LightGraphsMatching.jl.git",
+    target      = "build",
+    julia       = "0.6",
+    osname      = "linux"
+)
