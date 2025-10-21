2385959666d7409d2b9bb17c9ee80c024f7c7474
Simone Carlo Surace <51025924+simsurace@users.noreply.github.com>
2025-10-21 16:06:59 +0200

    Create API.jl

---
+++ docs/src/API.jl
@@ -0,0 +1,10 @@
+# API
+
+```@docs     
+GraphsMatching.minimum_weight_perfect_matching
+GraphsMatching.maximum_weight_maximal_matching
+GraphsMatching.LEMONMWPMAlgorithm
+GraphsMatching.LPAlgorithm
+GraphsMatching.HungarianAlgorithm
+GraphsMatching.BlossomVAlgorithm
+```
